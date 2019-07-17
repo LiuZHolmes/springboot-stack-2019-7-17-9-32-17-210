0257b20351ed6a51988c258bf1ff23430416825c
retr0 <395637649@qq.com>
2019-07-17 19:00:38 +0800

    story1 ac2

--- src/test/java/com/tw/apistackbase/CaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/CaseRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tw.apistackbase;
 
 import com.tw.apistackbase.entity.Case;
 import com.tw.apistackbase.repository.CaseRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
@@ -20,15 +21,32 @@ public class CaseRepositoryTest {
     @Autowired
     CaseRepository caseRepository;
 
+    private Case testCase;
+
+    @Before
+    public void setUp() throws Exception {
+        testCase = new Case("A_New_Case",new Date().getTime());
+    }
+
     @Test
     public void should_return_a_case_when_save_it() {
         // given
-        Case newCase = new Case("A_New_Case",new Date().getTime());
+
         // when
-        caseRepository.save(newCase);
+        caseRepository.save(testCase);
         // then
         List<Case> cases = caseRepository.findAll();
         Assertions.assertEquals(1,cases.size());
         Assertions.assertEquals("A_New_Case",cases.get(0).getCaseName());
+    }
+
+    @Test
+    public void should_return_a_case_when_find_case_by_id() {
+        // given
+        caseRepository.save(testCase);
+        // when
+        Case foundCase = caseRepository.findById(1L).orElse(null);
+        // then
+        Assertions.assertEquals("A_New_Case",foundCase.getCaseName());
     }
 }
